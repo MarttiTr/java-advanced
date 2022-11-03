3058085ab68c5b0dacfbc26e45905a57487553e6
Martti Triksberg <triksberg.martti@gmail.com>
2022-11-03 18:42:52 +0200

    Added list exercise

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -5,42 +5,42 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- *  Create a List and display its result (data should be provided by the user - console):
- *  a) Purchases to be made. *If an element already exists on the list, then it should
- *  not be added.
- *  b) *Add to the example above the possibility of "deleting" purchased elements
- *  c) Display only those purchases that start with „m” (e.g. milk)
- *  d) * View only purchases whose next product on the list starts with „m” (e.g. eggs,
- *  if milk was next on the list)
+ * Create a List and display its result (data should be provided by the user - console):
+ * a) Purchases to be made. *If an element already exists on the list, then it should
+ * not be added.
+ * b) *Add to the example above the possibility of "deleting" purchased elements
+ * c) Display only those purchases that start with „m” (e.g. milk)
+ * d) * View only purchases whose next product on the list starts with „m” (e.g. eggs,
+ * if milk was next on the list)
  */
-
 public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to the shop");
+
         List<String> shoppingBag = new ArrayList<>();
-
-        menuOperation( shoppingBag);
-
+        menuOperation(shoppingBag);
     }
 
     private static void menuOperation(List<String> shoppingBag) {
         int menuOption = getMenu();
-        System.out.println("-------------");
-        System.out.println("-----MENU-----");
-        System.out.println("-------------");
 
-        switch(menuOption) {
+        switch (menuOption) {
             case 1: // Add item
                 addItem(shoppingBag);
+                menuOperation(shoppingBag);
                 break;
             case 2: // Delete item
                 deleteItem(shoppingBag);
+                menuOperation(shoppingBag);
                 break;
-            case 3: // Display item
+            case 3: // Display items
                 displayItems(shoppingBag);
+                menuOperation(shoppingBag);
                 break;
-            case 4: // Exit
+            case 4: // exit
+                System.out.println("Thanks for shopping. Visit us again!");
                 break;
             default:
                 System.out.println("Incorrect option, choose the correct one!");
@@ -49,52 +49,51 @@ public class Main {
     }
 
     private static int getMenu() {
+        System.out.println("--------------");
+        System.out.println("     MENU    ");
+        System.out.println("--------------");
+
         List<String> menu = List.of("Add item", "Delete item", "Display items", "Exit");
 
-        for(int i = 0; i < menu.size(); i++) {
+        for (int i = 0; i < menu.size(); i++) {
             System.out.println(i + 1 + ". " + menu.get(i));
         }
 
         System.out.println("Choose a menu from above:");
-        
+
         return SCANNER.nextInt();
     }
-    
+
     private static void addItem(List<String> shoppingBag) {
         boolean isAdd = true;
-        System.out.println("Enter an item name to be added to the bag: ");
-        
-        while(isAdd) {
-            
+
+        while (isAdd) {
+            System.out.println("Enter an item name to be added to the bag: ");
             String addItem = SCANNER.next();
-        
-            if(!shoppingBag.contains(addItem)) {
+
+            if (!shoppingBag.contains(addItem)) {
                 shoppingBag.add(addItem);
-                System.out.println("Item added! Do you want to add more items?");
+                System.out.println("'" + addItem + "' added to the bag. Do you want to add more item? (true/false)");
             } else {
-                System.out.println("The item already exist, do you want to add an other item?");
-                isAdd = true;
-                }
+                System.out.println("The item already exists, do you want to add an other item? (true/false)");
+            }
 
             isAdd = SCANNER.nextBoolean();
-            }
-        
+        }
     }
 
     private static void deleteItem(List<String> shoppingBag) {
         boolean isDelete = true;
-        System.out.println("Enter an item name to be deleted from the bag: ");
 
-        while(isDelete) {
-
+        while (isDelete) {
+            System.out.println("Enter an item name to be deleted from the bag: ");
             String deleteItem = SCANNER.next();
 
-            if(shoppingBag.contains(deleteItem)) {
+            if (shoppingBag.contains(deleteItem)) {
                 shoppingBag.remove(deleteItem);
-                System.out.println("Item deleted! Do you want to delete more items?");
+                System.out.println("'" + deleteItem + "' deleted from the bag. Do you want to delete more item? (true/false)");
             } else {
-                System.out.println("The item not exist, do you want to delete an other item?");
-                isDelete = true;
+                System.out.println("The item not exists, do you want to delete an other item? (true/false)");
             }
 
             isDelete = SCANNER.nextBoolean();
@@ -104,14 +103,21 @@ public class Main {
     private static void displayItems(List<String> shoppingBag) {
         int counter = 1;
 
-        for(int i = 0; i < shoppingBag.size(); i++) {
-            String thisItem = shoppingBag.get(i);
-            String nextItem = (i + 1) == (shoppingBag.size() - 1) ? shoppingBag.get(i + 1) : "";
+        System.out.println("ITEMS: ");
 
-            if(thisItem.startsWith("m") || thisItem.startsWith("M") || nextItem.startsWith("m") || nextItem.startsWith("M")) {
-                System.out.println(counter + ", " + shoppingBag.get(i));
+        for (int i = 0; i < shoppingBag.size(); i++) {
+            String thisItem = shoppingBag.get(i);
+            String nextItem = (i + 1) <= (shoppingBag.size() - 1) ? shoppingBag.get(i + 1) : "";
+
+            if (thisItem.startsWith("m") || thisItem.startsWith("M") || nextItem.startsWith("m") || nextItem.startsWith("M")) {
+                System.out.println(counter + ". " + thisItem);
                 counter++;
             }
+        }
+
+
+        if (counter == 1) {
+            System.out.println("Your shopping bag is empty or doesn't contain the expected items. No items to display!");
         }
     }
 }
